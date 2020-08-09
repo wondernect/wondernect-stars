82cc7a4fa8287ef269383cc3b5f2d9694f95fccb
chenxun <sunbeamhome@163.com>
2020-08-09 17:55:07 +0800

    【m】初始化rbac、session、user server sdk

---
+++ rbac-server-sdk/src/main/java/com/wondernect/stars/App.java
@@ -0,0 +1,13 @@
+package com.wondernect.stars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
