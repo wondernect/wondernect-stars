7f5f0deeed205ee63e772627d8409ea1dbc0e9fd
530525 <530525@nd.com>
2020-11-06 17:00:08 +0800

    【m】更新包结构

---
+++ database/src/main/java/com/wondernect/stars/database/model/App.java
@@ -0,0 +1,12 @@
+package com.wondernect.stars.database.model;
+
+/**
+ * Copyright (C), 2017-2020, wondernect.com
+ * FileName: App
+ * Author: chenxun
+ * Date: 2020/11/6 16:59
+ * Description:
+ */
+public class App {
+
+}
