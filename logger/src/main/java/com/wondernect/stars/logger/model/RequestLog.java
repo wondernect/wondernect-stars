ebd3a605081c69b931b9e39426fde87dc9458650
530525 <530525@nd.com>
2020-12-08 19:54:59 +0800

    【m】完善request log日志记录

--- logger/src/main/java/com/wondernect/stars/logger/model/RequestLog.java
+++ logger/src/main/java/com/wondernect/stars/logger/model/RequestLog.java
@@ -7,9 +7,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 /**
  * Copyright (C), 2017-2020, wondernect.com
@@ -20,22 +18,25 @@ import javax.persistence.UniqueConstraint;
  */
 @Entity
 @Table(
-        name = "logger",
+        name = "request_log",
         uniqueConstraints = {@UniqueConstraint(columnNames = {"id"})}
 )
 @Data
 @EqualsAndHashCode(callSuper = false)
 @ApiModel(description = "日志")
-public class Logger extends BaseStringModel {
+public class RequestLog extends BaseStringModel {
 
+    @Column(columnDefinition = "not null")
     @JsonProperty("level")
     @ApiModelProperty(notes = "等级")
     private String level;
 
+    @Column(columnDefinition = "not null")
     @JsonProperty("service")
     @ApiModelProperty(notes = "服务")
     private String service;
 
+    @Column(columnDefinition = "not null")
     @JsonProperty("module")
     @ApiModelProperty(notes = "模块")
     private String module;
@@ -48,16 +49,52 @@ public class Logger extends BaseStringModel {
     @ApiModelProperty(notes = "操作用户名")
     private String userName;
 
+    @Column(columnDefinition = "not null")
     @JsonProperty("operation")
     @ApiModelProperty(notes = "操作")
     private String operation;
 
+    @Column(columnDefinition = "not null")
     @JsonProperty("description")
     @ApiModelProperty(notes = "日志描述")
     private String description;
 
-    @JsonProperty("trace_id")
-    @ApiModelProperty(notes = "链路跟踪id")
-    private String traceId;
+    @JsonProperty("request_id")
+    @ApiModelProperty(notes = "请求id")
+    private String requestId;
 
+    @JsonProperty("url")
+    @ApiModelProperty(notes = "请求url")
+    private String url;
+
+    @JsonProperty("method")
+    @ApiModelProperty(notes = "请求method")
+    private String method;
+
+    @Lob
+    @JsonProperty("arg_value")
+    @ApiModelProperty(notes = "请求参数")
+    private String argValue;
+
+    @Lob
+    @JsonProperty("return_value")
+    @ApiModelProperty(notes = "返回结果")
+    private String returnValue;
+
+    @JsonProperty("run_time")
+    @ApiModelProperty(notes = "运行时间")
+    private Long runTime;
+
+    @JsonProperty("ip")
+    @ApiModelProperty(notes = "客户端ip")
+    private String ip;
+
+    @JsonProperty("device_platform")
+    @ApiModelProperty(notes = "客户端平台")
+    private String devicePlatform;
+
+    @Lob
+    @JsonProperty("device_description")
+    @ApiModelProperty(notes = "客户端描述")
+    private String deviceDescription;
 }
