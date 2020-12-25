704456321af96ffb39b38ab939fdd6750d02a5f2
530525 <530525@nd.com>
2020-12-25 18:17:28 +0800

    【m】新增适配feign header context

---
+++ file-server/src/main/java/com/wondernect/stars/file/server/context/FileFeignHeaderContext.java
@@ -0,0 +1,16 @@
+package com.wondernect.stars.file.server.context;
+
+import com.wondernect.elements.feign.config.AbstractWondernectFeignHeaderContext;
+import org.springframework.stereotype.Service;
+
+/**
+ * Copyright (C), 2017-2020, wondernect.com
+ * FileName: PrtvuFeignHeaderContext
+ * Author: chenxun
+ * Date: 2020/12/25 13:56
+ * Description:
+ */
+@Service
+public class FileFeignHeaderContext extends AbstractWondernectFeignHeaderContext {
+
+}
--- sms/src/main/java/com/wondernect/stars/sms/model/SMSTemplateParam.java
+++ sms/src/main/java/com/wondernect/stars/sms/model/SMSTemplateParam.java
@@ -35,10 +35,6 @@ public class SMSTemplateParam extends BaseStringModel implements Serializable {
     @ApiModelProperty("短信模板")
     private String templateId;
 
-    @JsonProperty("name")
-    @ApiModelProperty("模板内容中变量名称")
-    private String name;
-
     @JsonProperty("param")
     @ApiModelProperty("传入替换数据Map<String, Object> varibles中key值")
     private String param;
